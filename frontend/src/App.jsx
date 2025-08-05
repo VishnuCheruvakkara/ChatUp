0faa5b33d26c4728cd9b3984da2da0fdc9c83b5f
vishnu cheruvakkara <vishnucheruvakkaraofficial@gmail.com>
2025-08-05 16:33:37 +0530

    start: project chatup initialized successfully.

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+
+    <>
+      <div className="bg-red-700 h-full ">
+
+        <h1 className="font-semibold text-white text-center " >Hello world</h1>
+      </div>
+
+    </>
+  )
+}
+
+export default App
