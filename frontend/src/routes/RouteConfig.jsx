1ecfbea100a99e16bf631903bafa672a9483ddbd
vishnu cheruvakkara <vishnucheruvakkaraofficial@gmail.com>
2025-08-17 15:06:54 +0530

    fix : front end path import fixed

--- frontend/src/routes/RouteConfig.jsx
+++ frontend/src/routes/RouteConfig.jsx
@@ -1,8 +1,8 @@
 import { Routes, Route } from 'react-router-dom';
 import PublicRoutes from './PublicRoutes';
 import AuthenticatedUserRoutes from './AuthenticatedUserRoutes';
-import ProtectPublicPath from './pathProtection/protectPublicPath';
-import ProtectAuthetictedPath from './pathProtection/protectAuthenticatedPath';
+import ProtectPublicPath from './pathProtection/ProtectPublicPath';
+import ProtectAuthetictedPath from './pathProtection/ProtectAuthenticatedPath';
 
 function RouteConfig() {
     return (
@@ -14,8 +14,7 @@ function RouteConfig() {
                 <Route path="/user/*" element={<AuthenticatedUserRoutes />} />
             </Route>
         </Routes>
-
     )
 }
 
-export default RouteConfig 
+export default RouteConfig
